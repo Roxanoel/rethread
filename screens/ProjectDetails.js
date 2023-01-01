b94f01c2d19992f03a56dab87e6cbc45bf1c23fd
Roxane Noël <roxane.noel92@gmail.com>
2023-01-01 08:55:17 -0500

    Created boilerplate for ProjectDetails screen

---
+++ screens/ProjectDetails.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function ProjectDetails() {
+    return (
+        <View>
+            <Text>This is the project details screen</Text>
+        </View>
+    )
+}
+
+export default ProjectDetails;
