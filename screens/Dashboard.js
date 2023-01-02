0fd4e140eb62216bebdce8a67ae03e0b81853b59
Roxane Noël <roxane.noel92@gmail.com>
2023-01-02 11:01:41 -0500

    Project title appears in header for project details

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -5,29 +5,35 @@ import Spacer from "../components/util/Spacer";
 import PALETTE from "../assets/PALETTE";
 
 function Dashboard({ route, navigation }) {
-    const { projectsData } = route.params;
+  const { projectsData } = route.params;
 
-    function renderProjectCard(project) {
-        function onPressHandler() {
-            navigation.navigate("ProjectDetails", {
-                project: project.item,
-              });
-        }
-
-        const color = PALETTE.primary200;  // Later, could be changed with project data if I want user to set a custom colour
-        
-        return <ProjectCard name={project.item.name} onPress={onPressHandler} color={color} />;
+  function renderProjectCard(project) {
+    function onPressHandler() {
+      navigation.navigate("ProjectDetails", {
+        project: project.item,
+      });
     }
-  
+
+    const color = PALETTE.primary200; // Later, could be changed with project data if I want user to set a custom colour
+
     return (
+      <ProjectCard
+        name={project.item.name}
+        onPress={onPressHandler}
+        color={color}
+      />
+    );
+  }
+
+  return (
     <View style={styles.listContainer}>
-        <FlatList
-      contentContainerStyle={styles.projectsList}
-      ItemSeparatorComponent={Spacer}
-      data={projectsData}
-      keyExtractor={(project) => project.id}
-      renderItem={renderProjectCard}
-    />
+      <FlatList
+        contentContainerStyle={styles.projectsList}
+        ItemSeparatorComponent={Spacer}
+        data={projectsData}
+        keyExtractor={(project) => project.id}
+        renderItem={renderProjectCard}
+      />
     </View>
   );
 }
@@ -39,7 +45,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   listContainer: {
-    flex: 1, 
-    padding: 16, 
-  }
+    flex: 1,
+    padding: 16,
+  },
 });
--- screens/ProjectDetails.js
+++ screens/ProjectDetails.js
@@ -1,8 +1,13 @@
 import { Text, View } from "react-native";
+import { useLayoutEffect } from "react";
 
-function ProjectDetails({ route }) {
+function ProjectDetails({ route, navigation }) {
     const projectData = route.params.project;
     
+    useLayoutEffect(() => {
+        navigation.setOptions({title: projectData.name});
+      }, [navigation, projectData]);
+
     return (
         <View>
             <Text>{projectData.name}</Text>
