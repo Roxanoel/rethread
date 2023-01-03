59d069a73033db3011caee0bdd10387185710c90
Roxane Noël <roxane.noel92@gmail.com>
2023-01-03 13:01:57 -0500

    Added custom icons to nav tabs

--- screens/ProjectDetails.js
+++ screens/ProjectDetails.js
@@ -1,10 +1,12 @@
 import { StyleSheet, View } from "react-native";
 import { useLayoutEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Entypo from "@expo/vector-icons/Entypo";
 
 import ProjectPlanning from "./ProjectPlanning";
 import ProjectProgress from "./ProjectProgress";
 import ProjectReview from "./ProjectReview";
+import PALETTE from "../assets/PALETTE";
 
 const BottomTab = createBottomTabNavigator();
 
@@ -17,10 +19,43 @@ function ProjectDetails({ route, navigation }) {
 
   return (
     <View style={styles.detailsContainer}>
-      <BottomTab.Navigator screenOptions={{headerShown: false}}>
-        <BottomTab.Screen name="Planning" component={ProjectPlanning} initialParams={{ projectData: projectData.planning}} />
-        <BottomTab.Screen name="Progress" component={ProjectProgress} initialParams={{ projectData: projectData.progress}} />
-        <BottomTab.Screen name="Review" component={ProjectReview} initialParams={{projectData: projectData.review}} />
+      <BottomTab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: PALETTE.primary500,
+          tabBarLabelStyle: { fontSize: 12 },
+        }}
+      >
+        <BottomTab.Screen
+          name="Planning"
+          component={ProjectPlanning}
+          initialParams={{ projectData: projectData.planning }}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Entypo name="colours" color={color} size={size} />
+            ),
+          }}
+        />
+        <BottomTab.Screen
+          name="Progress"
+          component={ProjectProgress}
+          initialParams={{ projectData: projectData.progress }}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Entypo name="pin" color={color} size={size} />
+            ),
+          }}
+        />
+        <BottomTab.Screen
+          name="Review"
+          component={ProjectReview}
+          initialParams={{ projectData: projectData.review }}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Entypo name="flickr-with-circle" color={color} size={size} />
+            ),
+          }}
+        />
       </BottomTab.Navigator>
     </View>
   );
@@ -29,7 +64,7 @@ function ProjectDetails({ route, navigation }) {
 export default ProjectDetails;
 
 const styles = StyleSheet.create({
-    detailsContainer: {
-        flex: 1,
-    }
+  detailsContainer: {
+    flex: 1,
+  },
 });
