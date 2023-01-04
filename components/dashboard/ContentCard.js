57c2e33644ba327ec915aac5e00bd7fcbae5d880
Roxane Noël <roxane.noel92@gmail.com>
2023-01-04 09:42:53 -0500

    Wrote basis of reusable card component

---
+++ components/dashboard/ContentCard.js
@@ -0,0 +1,29 @@
+import { View, StyleSheet } from "react-native";
+import PALETTE from "../../assets/PALETTE";
+
+function ContentCard({ children, title, color }) {
+    const defaultColor = PALETTE.primary200;
+  
+    return (
+    <View style={[styles.card, { backgroundColor: color? color: defaultColor }]}>
+      <Text style={styles.cardTitle}>{title}</Text>
+      {children}
+    </View>
+  );
+}
+
+export default ContentCard;
+
+const styles = StyleSheet.create({
+    card: {
+      padding: 16,
+      width: "100%",
+      borderRadius: 16,
+      minHeight: 96, // For now
+    },
+    cardTitle: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      color: PALETTE.primary700 // For now, add support for customizing later 
+    }
+  });
