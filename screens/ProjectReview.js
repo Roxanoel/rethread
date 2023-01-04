000d675e222966886e0f814bf2c0877d7594dab3
Roxane Noël <roxane.noel92@gmail.com>
2023-01-04 10:51:47 -0500

    Added basic cards + data for review tab

--- screens/ProjectReview.js
+++ screens/ProjectReview.js
@@ -1,15 +1,39 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView, StyleSheet } from "react-native";
 
+import ContentCard from "../components/dashboard/ContentCard";
 import DetailsHeader from "../components/project-details/DetailsHeader";
 
 function ProjectReview({ route, navigation }) {
-    const projectData = route.params.projectData;
-    
-    return (
-        <View style={{flex: 1}}>
-            <DetailsHeader title="Review" />
-            <Text>{JSON.stringify(projectData)}</Text>
-        </View>
-    )}
+  const projectData = route.params.projectData;
+
+  return (
+    <View style={{ flex: 1 }}>
+      <ScrollView style={styles.cardsContainer}>
+      <DetailsHeader title="Review" />
+        <ContentCard title="Images">
+          <Text>{JSON.stringify(projectData.images)}</Text>
+        </ContentCard>
+        <ContentCard title="What Went Well">
+          <Text>{projectData.whatWentWell}</Text>
+        </ContentCard>
+        <ContentCard title="What I learned">
+          <Text>{projectData.learned}</Text>
+        </ContentCard>
+        <ContentCard title="What I Want to Improve">
+          <Text>{projectData.whatToImprove}</Text>
+        </ContentCard>
+        <ContentCard title="Would I Do it Again?">
+          <Text>{projectData.wouldRedo ? "yes" : "no"}</Text>
+        </ContentCard>
+      </ScrollView>
+    </View>
+  );
+}
 
 export default ProjectReview;
+
+const styles = StyleSheet.create({
+    cardsContainer: {
+        paddingHorizontal: 8,
+    }
+});
